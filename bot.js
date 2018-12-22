4e33722c8b4714d8234cadecb88e8073ae9d22e6
Brad <brad2142@gmail.com>
2018-12-22 15:25:50 -0800

    switched to discord.js syntax and added voice channel connection support

--- bot.js
+++ bot.js
@@ -1,88 +1,82 @@
-const Discord = require('discord.io');
+const Discord = require('discord.js');
 const logger = require('winston');
 const auth = require('./auth.json');
+const yt = require('ytdl-core');
+const pkg = require('./package.json');
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
+
 // Initialize Discord Bot
-const bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
-});
-bot.on('ready', function (evt) {
+const bot = new Discord.Client();
+
+bot.login(auth.token);
+
+bot.on('ready', () => {
     logger.info('Connected');
     logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info(pkg.name + ' - (' + pkg.author + ')');
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
+
+bot.on('message', message => {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
+    if (message.content.substring(0, 1) == '!') {
+        var args = message.content.substring(1).split(' ');
         var cmd = args[0];
 
         args = args.splice(1);
         switch(cmd) {
             // !ping
             case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
+                message.reply('Pong!');
             break;
 
             // !greet greets the user
             case 'greet':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Hello, ' + user + '.'
-                });
+                message.reply('Hello, ' + message.member.nickname + '.');
             break;
 
             // !rolldice will roll a random 6-sided die and output a message with the result
             case 'rolldice':
                 var rand = Math.floor(Math.random() * 6) + 1;
                 if (rand == 1) {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'Awwww you rolled a 1. Better luck next time!'
-                    });
+                    message.reply('Awwww you rolled a 1. Better luck next time!');
                 }
                 if (rand == 2) {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'You rolled a 2. Could have been better I guess!'
-                    });
+                    message.reply('You rolled a 2. Could have been better I guess!');
                 }
                 if (rand == 3) {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'You rolled a 3. Not bad!'
-                    });
+                    message.reply('You rolled a 3. Not bad!');
                 }
                 if (rand == 4) {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'You rolled a 4. I guess it\'s alright.'
-                    });
+                    message.reply('You rolled a 4. I guess it\'s alright.');
                 }
                 if (rand == 5) {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'You rolled a 5! Nice!'
-                    });
+                    message.reply('You rolled a 5! Nice!');
                 }
                 if (rand == 6) {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: 'Wow! You rolled a 6! It must be your lucky day :)'
-                    });
+                    message.reply('Wow! You rolled a 6! It must be your lucky day :)');
                 }
             break;
 
+            // enter current voice channel and start playing songs
+            case 'stream':
+                if (!message.guild) return;
+                if (message.member.voiceChannel) {
+                    message.member.voiceChannel.join()
+                        .then(connection => {
+                            message.reply('SUCCESSFULLY CONNECTED BEEP BOOP');
+                        })
+                        .catch(console.log);
+                } else {
+                    message.reply('You need to join a voice channel first!');
+                }
+            break;
          }
      }
 });
